0cefbaff0d4456e1a118135e0ffe9198b0cbd709
zenocodes <azronebrian@gmail.com>
2022-10-03 14:31:11 +0300

    configured db

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 import express from 'express'
+import mysql from 'mysql'
 
 const app = express()
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'posty'
+})
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    render('index')
+    res.render('index')
+})
+
+// login: display form
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+// login: submit form
+app.post('/login', (req, res) => {
+
+})
+
+// signup: display form
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+// signup: submit form
+app.post('/signup', (req, res) => {
+
 })
 
 const PORT = process.env.PORT || 3000
